a9d8066ed4947fb04b1ca8242ffef6fd5e50d40d
Greg <gjmooney@asu.edu>
2023-10-08 19:38:02 +0200

    Remove div

--- src/components/sidenav/SideNav.tsx
+++ src/components/sidenav/SideNav.tsx
@@ -27,93 +27,91 @@ interface SideNavProps {}
 
 const SideNav = ({}: SideNavProps) => {
   return (
-    <nav className="absolute left-0 w-80 bg-green-600 h-screen text-muted">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline">Open</Button>
-        </SheetTrigger>
-        <SheetContent className="text-muted-foreground" side="left">
-          <SheetHeader>
-            <SheetTitle>Tardigrade</SheetTitle>
-          </SheetHeader>
-          <div className="flex flex-col gap-4 py-4">
-            <SearchCommand />
+    <Sheet defaultOpen>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent className="text-muted-foreground" side="left">
+        <SheetHeader>
+          <SheetTitle>Tardigrade</SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col gap-4 py-4">
+          <SearchCommand />
 
-            <div className="flex flex-col space-y-2 justify-start items-center">
-              <Link
-                href="/"
-                className="flex items-center gap-2 px-3 h-8 shrink-0 hover:text-primary hover:bg-secondary w-full"
-              >
-                <Home className="w-10 h-10 p-2" />
-                <span className="font-medium">Home</span>
-              </Link>
-              <Link
-                href="/"
-                className="flex items-center gap-2 px-3 h-8 shrink-0 hover:text-primary hover:bg-secondary w-full"
-              >
-                <Bell className="w-10 h-10 p-2" />
-                <span className="font-medium">Notifications</span>
-              </Link>
-              <Link
-                href="/"
-                className="flex items-center gap-2 px-3 h-8 shrink-0 hover:text-primary hover:bg-secondary w-full"
-              >
-                <Trophy className="w-10 h-10 p-2" />
-                <span className="font-medium">Goals</span>
-              </Link>
-            </div>
-
-            <Accordion type="single" collapsible className="w-full">
-              <Separator />
-
-              <AccordionItem
-                value="favorites"
-                className="hover:text-primary hover:bg-muted"
-              >
-                <AccordionTrigger>Favorites</AccordionTrigger>
-                <AccordionContent>
-                  <Favorites />
-                </AccordionContent>
-              </AccordionItem>
-
-              <AccordionItem
-                value="spaces"
-                className="hover:text-primary hover:bg-muted"
-              >
-                <AccordionTrigger>Spaces</AccordionTrigger>
-                <AccordionContent>
-                  <Spaces />
-                </AccordionContent>
-              </AccordionItem>
-
-              <AccordionItem
-                value="dashboards"
-                className="hover:text-primary hover:bg-muted"
-              >
-                <AccordionTrigger>Dashboards</AccordionTrigger>
-                <AccordionContent>
-                  <Dashboards />
-                </AccordionContent>
-              </AccordionItem>
-
-              <AccordionItem
-                value="docs"
-                className="hover:text-primary hover:bg-muted"
-              >
-                <AccordionTrigger>Docs</AccordionTrigger>
-                <AccordionContent>
-                  <Docs />
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
+          <div className="flex flex-col space-y-2 justify-start items-center">
+            <Link
+              href="/"
+              className="flex items-center gap-2 px-3 h-8 shrink-0 hover:text-primary hover:bg-secondary w-full"
+            >
+              <Home className="w-10 h-10 p-2" />
+              <span className="font-medium">Home</span>
+            </Link>
+            <Link
+              href="/"
+              className="flex items-center gap-2 px-3 h-8 shrink-0 hover:text-primary hover:bg-secondary w-full"
+            >
+              <Bell className="w-10 h-10 p-2" />
+              <span className="font-medium">Notifications</span>
+            </Link>
+            <Link
+              href="/"
+              className="flex items-center gap-2 px-3 h-8 shrink-0 hover:text-primary hover:bg-secondary w-full"
+            >
+              <Trophy className="w-10 h-10 p-2" />
+              <span className="font-medium">Goals</span>
+            </Link>
           </div>
 
-          <SheetFooter className="absolute bottom-0 left-0 border-t h-12 w-full">
-            <BottomBar />
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
-    </nav>
+          <Accordion type="single" collapsible className="w-full">
+            <Separator />
+
+            <AccordionItem
+              value="favorites"
+              className="hover:text-primary hover:bg-muted"
+            >
+              <AccordionTrigger>Favorites</AccordionTrigger>
+              <AccordionContent>
+                <Favorites />
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem
+              value="spaces"
+              className="hover:text-primary hover:bg-muted"
+            >
+              <AccordionTrigger>Spaces</AccordionTrigger>
+              <AccordionContent>
+                <Spaces />
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem
+              value="dashboards"
+              className="hover:text-primary hover:bg-muted"
+            >
+              <AccordionTrigger>Dashboards</AccordionTrigger>
+              <AccordionContent>
+                <Dashboards />
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem
+              value="docs"
+              className="hover:text-primary hover:bg-muted"
+            >
+              <AccordionTrigger>Docs</AccordionTrigger>
+              <AccordionContent>
+                <Docs />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+
+        <SheetFooter className="absolute bottom-0 left-0 border-t h-12 w-full">
+          <BottomBar />
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
 
