a55c374ed21df071e2770d5cec8170d42710a7cf
Greg <gjmooney@asu.edu>
2023-10-11 13:38:04 +0200

    Create schema

---
+++ src/db/schema.ts
@@ -0,0 +1,38 @@
+import { relations, sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  clerkId: text("clerk_id").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`CURRENT_TIMESTAMP`
+  ),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  tasks: many(tasks),
+}));
+
+export const tasks = sqliteTable("tasks", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  description: text("description"),
+  category: text("category", {
+    enum: ["todo", "inprogress", "test", "complete"],
+  }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`CURRENT_TIMESTAMP`
+  ),
+  updatedAt: integer("created_at", { mode: "timestamp" }),
+  createdById: integer("created_by_id").notNull(),
+});
+
+export const tasksRelation = relations(tasks, ({ one }) => ({
+  createdBy: one(users, {
+    fields: [tasks.createdById],
+    references: [users.id],
+  }),
+}));
+
+export type User = typeof users.$inferSelect;
+export type Task = typeof tasks.$inferSelect;
