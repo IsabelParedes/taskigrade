7b71df91cfcacae76b22b588ddda0ddc3b58f1d8
Greg <gjmooney@asu.edu>
2023-10-14 18:14:36 +0200

    Remove temp data

---
+++ src/lib/constants.ts
@@ -0,0 +1,20 @@
+import { Column } from "@/types/types";
+
+export const statusCols: Column[] = [
+  {
+    id: "todo",
+    title: "To Do",
+  },
+  {
+    id: "inprogress",
+    title: "In Progress",
+  },
+  {
+    id: "test",
+    title: "Test",
+  },
+  {
+    id: "complete",
+    title: "Complete",
+  },
+];
