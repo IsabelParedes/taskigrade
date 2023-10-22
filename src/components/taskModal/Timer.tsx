c4cce5c751d0a8b7d9931d1ada6d4998e365e125
gjmooney <gjmooney@asu.edu>
2023-10-22 14:33:13 +0200

    Add leading 0's to timer display

--- src/components/taskModal/Timer.tsx
+++ src/components/taskModal/Timer.tsx
@@ -53,9 +53,12 @@ const Timer = ({ taskId, totalTime }: TimerProps) => {
   };
 
   const formatTime = (timeInSeconds: number): string => {
-    const hours = Math.floor(timeInSeconds / 3600);
-    const minutes = Math.floor((timeInSeconds % 3600) / 60);
-    const seconds = timeInSeconds % 60;
+    const hours = String(Math.floor(timeInSeconds / 3600)).padStart(2, "0");
+    const minutes = String(Math.floor((timeInSeconds % 3600) / 60)).padStart(
+      2,
+      "0"
+    );
+    const seconds = String(timeInSeconds % 60).padStart(2, "0");
     return `${hours}:${minutes}:${seconds}`;
   };
 
